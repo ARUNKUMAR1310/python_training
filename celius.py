04de9f3694b2f8a1345276c8a7c40df68ccb37ea
ARUNKUMAR1310 <159912077+ARUNKUMAR1310@users.noreply.github.com>
2025-03-15 12:17:28 +0530

    Fahrenheit&celius

---
+++ celius.py
@@ -0,0 +1,7 @@
+celius=float(input("Enter the celius:"))
+Fahrenheit=(celius*1.8)+32
+print(Fahrenheit)
+
+Fahrenheit=float(input("Enter the Fahrenheit:"))
+celius=(Fahrenheit-32)*0.56
+print(celius)
